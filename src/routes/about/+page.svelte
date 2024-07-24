a176ec1800046b0fea49b6f497dd549f97ac747e
DoAn08 <truongancpp8@gmail.com>
2024-07-24 20:22:45 +0700

    add mobile support

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,7 +17,7 @@
             welcome to my world
         </p>
         <p style="color: rgba(0, 0, 0, 0.75); margin-bottom: 32px; text-align: left;">
-            "the idea is to create a place where i can honestly convey everything happening inside my head, free from surveillance of greedy conglomerate. remember all of this is written from my own standpoint and there are bound to be disagreements, however, feel free to leave comments under each of my posts stating that disagreements as i believe that by that way, we can help each other to expand our horizons. there's also a password system in place, protecting thoughts that i fear would tarnish my public reputations and the few whom i trusted to share it with."
+            "the idea is to create a place where i can honestly convey everything happening inside my head, free from surveillance of greedy conglomerates. remember all of this is written from my own standpoint and there are bound to be disagreements, however, feel free to leave comments under each of my posts stating that disagreements as i believe we can help each other to expand our horizons by that way. there's also a password system in place, protecting thoughts that i fear would tarnish my public reputations and the few whom i trusted to share it with."
         </p>
         <p style="font-size: small; margin-left: 48px; margin-right: 48px; margin-top: 24px; color: rgba(0, 0, 0, 0.5)">
             this would not happen without the foundations already laid for me by open-source projects and code snippets made by developer worldwide. to this, you all have my gratitude and i truly hope you are successful with your life.
